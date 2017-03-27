b5be4203d41671f5ec68e3a3c91edeae4d2f87ba
Laura <laurazenc@gmail.com>
2017-03-27 11:50:53 +0200

    Included conection to woocommerce api

--- config.js
+++ config.js
@@ -1,146 +1,91 @@
 {
-name: "WooCommerce Connection Service",
-identifier: "woocommerceconnection",
-description: {
-es-ES: "",
-en-US: ""
-},
-documentation: {
-es-ES: "services/woocommerceconnection/documentation/es_ES.md",
-en-US: "services/woocommerceconnection/documentation/en_US.md"
-},
-descriptionShort: {
-es-ES: "",
-en-US: ""
-},
-type: "A",
-version: "0.0.0",
-author: "King of App",
-category: [
-"payment"
-],
-price: 0,
-subscription: true,
-platforms: [
-"android",
-"ios",
-"windows"
-],
-showOn: {
-market: true
-},
-events: {
-provide: [
-"payment"
-]
-},
-requires: [ ],
-deps: [
-"com.paypal.cordova.mobilesdk"
-],
-vendor: {
-},
-images: {
-popover: "services/woocommerceconnection/images/banner.png",
-banner: "services/woocommerceconnection/images/banner.png",
-logo: "services/woocommerceconnection/images/logo.png",
-list: "services/woocommerceconnection/images/list.png",
-screenshots: [
-"services/woocommerceconnection/images/screenshot01.png"
-]
-},
-scope: {
-clientId: "",
-sandboxClientId: "",
-brandName: "",
-brandEmail: "",
-agreementUrl: "http://mypage.com/agreement",
-policyUrl: "http://mypage.com/policy",
-sandbox: true,
-acceptCreditCards: true,
-preferences: {
-country: {
-us: 1,
-es: 1
-},
-currency: {
-EUR: 1,
-USD: 0.8
-},
-basePriority: 1
-}
-},
-config: [
-{
-type: "input",
-key: "clientId",
-templateOptions: {
-label: "client ID",
-required: true,
-description: "Paypal client ID"
-}
-},
-{
-type: "input",
-key: "sandboxClientId",
-templateOptions: {
-label: "sandboxClientId",
-required: true,
-description: "Paypal sandbox client ID (only if sandbox is true)"
-}
-},
-{
-type: "input",
-key: "brandName",
-templateOptions: {
-label: "brandName",
-required: true,
-description: "Name of your brand"
-}
-},
-{
-type: "input",
-key: "brandEmail",
-templateOptions: {
-type: "email",
-label: "brandEmail",
-required: true,
-description: "Email of your brand"
-}
-},
-{
-type: "input",
-key: "agreementUrl",
-templateOptions: {
-description: "URL of your company's user agreement, which will be offered to the user when requesting consent via a PayPalFuturePaymentViewController.",
-pattern: "(https?://)([/\w.()-]*).*",
-required: true,
-label: "Agreement url"
-}
-},
-{
-type: "input",
-key: "policyUrl",
-templateOptions: {
-description: "URL of your company's privacy policy, which will be offered to the user when requesting consent via a PayPalFuturePaymentViewController.",
-pattern: "(https?://)([/\w.()-]*).*",
-required: true,
-label: "Policy url"
-}
-},
-{
-type: "checkbox",
-key: "sandbox",
-templateOptions: {
-label: "Check to use sandbox mode (only in dev environment)"
-}
-},
-{
-type: "checkbox",
-key: "acceptCreditCards",
-templateOptions: {
-label: "The SDK will support both payment types, PayPal or credit card"
-}
-}
-]
+    "name": "WooCommerce Connection Service",
+    "identifier": "woocommerceconnection",
+    "description": {
+        "es-ES": "",
+        "en-US": ""
+    },
+    "documentation": {
+        "es-ES": "services/woocommerceconnection/documentation/es_ES.md",
+        "en-US": "services/woocommerceconnection/documentation/en_US.md"
+    },
+    "descriptionShort": {
+        "es - ES": "",
+        "en - US": ""
+    },
+    "type": "A",
+    "version": "0.0.0",
+    "author": "King of App",
+    "category": [
+        "payment"
+    ],
+    "price": 0,
+    "subscription": true,
+    "platforms": [
+        "android",
+        "ios",
+        "windows"
+    ],
+    "files": [
+      "services/woocommerceconnection/oauth/hmac-sha1.js",
+      "services/woocommerceconnection/oauth/enc-base64-min.js",
+      "services/woocommerceconnection/oauth/oauth1.0a.js",
+      "services/woocommerceconnection/service.js"
+    ],
+    "libs": [{
+      "bower": {
+        "oauth-signature": "^1.3.1"
+      },
+      "src": "bower_components/oauth-signature/dist/oauth-signature.js"
+    }],
+    "showOn": {
+        "market": true
+    },
+    "requires": [],
+    "deps": [
+        "com.paypal.cordova.mobilesdk"
+    ],
+    "images": {
+        "popover": "services/woocommerceconnection/images/banner.png",
+        "banner": "services/woocommerceconnection/images/banner.png",
+        "logo": "services/woocommerceconnection/images/logo.png",
+        "list": "services/woocommerceconnection/images/list.png",
+        "screenshots": [
+            "services/woocommerceconnection/images/screenshot01.png"
+        ]
+    },
+    "scope": {
+      "url": "",
+      "consumerKey" : "",
+      "consumerSecretKey": ""
+    },
+    "config": [
+      {
+        "type": "input",
+        "key": "url",
+        "templateOptions": {
+          "label": "URL",
+          "required": true,
+          "description": "Store URL"
+        }
+      },
+      {
+        "type": "input",
+        "key": "consumerKey",
+        "templateOptions": {
+          "label": "Consumer Key",
+          "required": true,
+          "description": "Consumer Key"
+        }
+      },
+      {
+        "type": "input",
+        "key": "consumerSecretKey",
+        "templateOptions": {
+          "label": "Consumer Secret Key",
+          "required": true,
+          "description": "Consumer Secret Key"
+        }
+      }
+    ]
 }
--- service.js
+++ service.js
@@ -2,11 +2,136 @@ angular
   .module('king.services.woocommerceconnection', [])
   .run(woocommerceconnectionService);
 
-  woocommerceconnectionService.$inject = [];
-  function woocommerceconnectionService() {
+  woocommerceconnectionService.$inject = ['$q', 'configService', '$http'];
+  function woocommerceconnectionService($q, configService, $http) {
     var scope        = configService.services.woocommerceconnection.scope;
-
     console.log('[V] Loading WooCommerceConnection service...');
 
+    var route          = '';
+    var request        = {  url     : scope.url + 'wc-api/v3/',
+                            method  : 'GET'
+                         };
+    var isAmpersand    = false;
+    var orderList      = {};
+    var oauth          = OAuth({
+                            consumer: {
+                                key   : scope.consumerKey,
+                                secret: scope.consumerSecretKey
+                            },
+                            signature_method: 'HMAC-SHA1',
+                            hash_function: function(base_string, key) {
+                                return crypto.createHmac('sha1', key).update(base_string).digest('base64');
+                            }
+                          });
+    var oauth_data     = {  oauth_consumer_key    : oauth.consumer.key,
+                            oauth_nonce           : oauth.getNonce(5),
+                            oauth_signature_method: oauth.signature_method,
+                            oauth_timestamp       : oauth.getTimeStamp()
+                          };
+                              
+    return {
+      getProducts         : getProducts,
+      getProductById      : getProductById,
+      getProductReviews   : getProductReviews,
+      getProductByCategory: getProductByCategory,
+      getProductByTag     : getProductByTag,
+      getProductAttributes: getProductAttributes,
+      getOrder            : getOrder
+    };
+
+    function getProducts() {
+      route       = "products";
+      var results = getResults(route);
+      return results;
+    }
+
+    function getProductById(productId) {
+      route       = "products/" + productId;
+      var results = getResults(route);
+      return results;
+    }
+
+    function getProductReviews(productId) {
+      route       = "products/"+ productId +"/reviews";
+      var results = getResults(route);
+      return results;
+    }
+
+    // slug category
+    function getProductByCategory(category) {
+      route       = "products?";
+      isAmpersand = true;
+      oauth_data['filter[category]'] = category;
+      var results = getResults(route);
+      return results;
+    }
+
+    function getProductByTag(tag) {
+      route       = "products?";
+      isAmpersand = true;
+      oauth_data['filter[tag]'] = tag;
+      var results = getResults(route);
+      return results;
+    }
+
+    function getProductAttributes(productId) {
+      route       = "products/attributes/"+ productId;
+      var results = getResults(route);
+      return results;
+    }
+
+    function createOrder(orderItem) {
+      route          = "orders";
+      request.method = "POST";
+      var results    = postOrder(route, orderItem);
+      return results;
+    }
+
+    function getOrder(orderId) {
+      route       = "orders/" + orderId;
+      var results = getResults(route);
+      return results;
+    }
+
+    function getResults(route, isAmpersand) {
+      var deferred               = $q.defer();
+      var url                    = request.url + route;
+      oauth_data.oauth_signature = oauthSignature.generate(request.method, url, oauth_data, oauth.consumer.secret, '', { encodeSignature: true});
+      var encodedUrl             = getEncodedUrl(route);
+      $http({
+          method    : request.method,
+          url       : encodedUrl,
+          headers   : {
+            "Content-Type": "application/json; charset=UTF-8"
+          }
+      })
+      .then(function(objS){
+        deferred.resolve(objS);
+      })
+      .catch(function(objE){
+        deferred.reject(objE);
+      });
+      return deferred.promise;
+    }
+
+    oauth.getTimeStamp = function() {
+      return Math.round((new Date()).getTime() / 1000.0);
+    }
+
+    oauth.getNonce = function(lenght) {
+        var text     = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        for( var i=0; i < lenght; i++ )
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        return text;
+    }
+
+    function getEncodedUrl(route, order) {
+      var paramsStr                = oauth.getParameterString(request, oauth_data);
+      var requestUrl               = request.url + route;
+      requestUrl                   = (isAmpersand) ? requestUrl + '&' : requestUrl + '?';
+      var encodedUrl               = requestUrl + paramsStr;
+      return encodedUrl;
+    }
 
   }
